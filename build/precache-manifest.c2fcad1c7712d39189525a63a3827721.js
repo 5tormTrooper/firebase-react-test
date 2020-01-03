73c534c4c89d66b3f3ccb4e8abe446e76250127f
Storm Farrell <email@stormfarrell.com>
2020-01-03 11:29:57 +0100

    Add build folder

---
+++ build/precache-manifest.c2fcad1c7712d39189525a63a3827721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f55e2eecd7e00527c2ef1558836397a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "710579420a30baf2ddf4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1dfc05316972ca07eed4",
+    "url": "/static/js/2.d505424d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d505424d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "710579420a30baf2ddf4",
+    "url": "/static/js/main.71c0bd09.chunk.js"
+  },
+  {
+    "revision": "801e631941ac66f4fd08",
+    "url": "/static/js/runtime-main.b7f38194.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
